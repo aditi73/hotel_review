3ba2cc26bbeec17a391d6a4bc06d579aaa8f3661
kmaheshwari <a.thakur73.at@gmail.com>
2016-09-28 10:57:26 +0530

    remove extra code

--- hotel_review_scrap.rb
+++ hotel_review_scrap.rb
@@ -12,8 +12,12 @@ begin
 	@review_count = @page.css("span.tabs_pers_counts")[0].text.gsub(/[()]/,'').to_i
 	@total_pages = @page.css("a.pageNum").last.text.to_i
 
-	CSV.open("hotel.csv", "wb") do |csv|
+	#open csv file
+	CSV.open("myhotel.csv", "wb") do |csv|
 		csv << ["Review Url", "Review Title" , "Review Text", "Review Response", "Review Date", "Customer Review Count","Customer Name", "Customer Location"]
+		csv << []
+
+		#reviews info
 		(0..(@total_pages-1)).each do |i|
 			@review_page=Nokogiri::HTML(open("https://www.tripadvisor.in/Hotel_Review-g297628-d2038751-Reviews-or"+(i*10).to_s+"-Ibis_Bengaluru_Techpark-Bengaluru_Bangalore_Karnataka",:read_timeout => 10000000000000000))
 			
@@ -24,24 +28,9 @@ begin
 				else
 					@review_url = @review_exist.attr('href')
 					@review_title = @review_page.css('span.noQuotes')[j].text
-					# if !@review_t.nil?
-					# 	@review_title = @review_t.text
-					# else
-					# 	@review_title = "Title Not Fount"
-					# end
 					@review_text = @review_page.css('div.entry p.partial_entry')[j].text.gsub!(/[^0-9A-Za-z' ''@,.]/, '')
-					# if !@r_text.nil?
-						# @review_text = @r_text.text.gsub!(/[^0-9A-Za-z' ''@,.]/, '')
-					# else
-					# 	@review_text = "No review text"
-					# end
 					@response = @review_page.css('div.mgrRspnInline p.partial_entry')[j]
 					@review_date = @review_page.css('span.ratingDate')[j].attr('title')
-					# if !@r_date.nil?
-					# 	@review_data = @r_date.attr('title')
-					# else
-					# 	@review_date = "Review date not found"
-					# end
 					@cust_review = @review_page.css('div.reviewerBadge span.badgeText')[j]
 					if !@cust_review.nil?
 						@total_cust_review = @cust_review.text
@@ -53,17 +42,7 @@ begin
 					else
 						@mem_info = @review_page.css('div.username')[j].text.gsub!(/[^0-9A-Za-z' '',.@]/, '')
 					end
-					# if !@mem_name.nil?
-					# 	@mam_info =@mem_name.text.gsub!(/[^0-9A-Za-z' '',.@]/, '')
-					# else
-					# 	@mem_info = "No Name"
-					# end
 					@location = @review_page.css('div.location')[j].text.gsub!(/[^0-9A-Za-z' '',.@]/, '')
-					# if !@loc.nil?
-					# 	@location = @loc.text.gsub!(/[^0-9A-Za-z' '',.@]/, '')
-					# else
-					# 	@location = "Location not found"
-					# end
 					if !@response.nil?
 						@review_data = Nokogiri::HTML(open("https://www.tripadvisor.in"+@review_url))
 						@review_res = @review_data.css("div.displayText")[0]
@@ -83,7 +62,7 @@ begin
 		end
 	end
 rescue
-	p "Internet issue"
+	p "Socket error"
 end
 
 
